63833be165a0a0eea908f8b47d0ab77f52c8387a
Spit437 <spit437@gmial.com>
2016-01-27 23:32:42 +0300

    добавлены переходы на сайты с гугл формами для кнопок: -Сообщить о проблеме -Отправить предложение -Отправить отзыв всё работает поэтому пушу в мастер

--- heatLoss/heatLoss/Form1.cs
+++ heatLoss/heatLoss/Form1.cs
@@ -22,5 +22,20 @@ namespace heatLoss
             AboutBox1 aboutBox = new AboutBox1();
             aboutBox.Show();
         }
+
+        private void сообщитьОПролемеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/1Rvw2X3JUEZhitx8P5W06GPXulvBK2lRabc4j1MjAYbo/viewform?usp=send_form");
+        }
+
+        private void отправитьОтзывToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/11tuqMnsxrRshlET-zbtlzCHDYGETmZbX_RNNbm3DTsk/viewform?usp=send_form");
+        }
+
+        private void отправитьПредложениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/19d-rJdhVJezor4cf6tUDdQU5WJesmW7kTAM-NeCcshI/viewform?usp=send_form");
+        }
     }
 }
