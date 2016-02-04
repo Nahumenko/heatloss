c9ee1f7cd6b698924f0ff771d36fc436c79a46d4
Echishev <aleksandr.echishev@yandex.ru>
2016-02-04 22:27:22 +0300

    На форму "редактирование справочных таблиц" добавил формы для таблиц

--- heatLoss/heatLoss/EditorReferenceTables.Designer.cs
+++ heatLoss/heatLoss/EditorReferenceTables.Designer.cs
@@ -28,19 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.температурыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.наружныйДиаментрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.условныйДиаметрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.объемВодыВСистемеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.теплопотериToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.температурыToolStripMenuItem,
+            this.наружныйДиаментрToolStripMenuItem,
+            this.условныйДиаметрToolStripMenuItem,
+            this.объемВодыВСистемеToolStripMenuItem,
+            this.теплопотериToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(697, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // температурыToolStripMenuItem
+            // 
+            this.температурыToolStripMenuItem.Name = "температурыToolStripMenuItem";
+            this.температурыToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.температурыToolStripMenuItem.Text = "Температуры";
+            this.температурыToolStripMenuItem.Click += new System.EventHandler(this.температурыToolStripMenuItem_Click);
+            // 
+            // наружныйДиаментрToolStripMenuItem
+            // 
+            this.наружныйДиаментрToolStripMenuItem.Name = "наружныйДиаментрToolStripMenuItem";
+            this.наружныйДиаментрToolStripMenuItem.Size = new System.Drawing.Size(128, 20);
+            this.наружныйДиаментрToolStripMenuItem.Text = "Наружный диаметр";
+            this.наружныйДиаментрToolStripMenuItem.Click += new System.EventHandler(this.наружныйДиаментрToolStripMenuItem_Click);
+            // 
+            // условныйДиаметрToolStripMenuItem
+            // 
+            this.условныйДиаметрToolStripMenuItem.Name = "условныйДиаметрToolStripMenuItem";
+            this.условныйДиаметрToolStripMenuItem.Size = new System.Drawing.Size(124, 20);
+            this.условныйДиаметрToolStripMenuItem.Text = "Условный диаметр";
+            this.условныйДиаметрToolStripMenuItem.Click += new System.EventHandler(this.условныйДиаметрToolStripMenuItem_Click);
+            // 
+            // объемВодыВСистемеToolStripMenuItem
+            // 
+            this.объемВодыВСистемеToolStripMenuItem.Name = "объемВодыВСистемеToolStripMenuItem";
+            this.объемВодыВСистемеToolStripMenuItem.Size = new System.Drawing.Size(145, 20);
+            this.объемВодыВСистемеToolStripMenuItem.Text = "Объем воды в системе";
+            this.объемВодыВСистемеToolStripMenuItem.Click += new System.EventHandler(this.объемВодыВСистемеToolStripMenuItem_Click);
+            // 
+            // теплопотериToolStripMenuItem
+            // 
+            this.теплопотериToolStripMenuItem.Name = "теплопотериToolStripMenuItem";
+            this.теплопотериToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.теплопотериToolStripMenuItem.Text = "Теплопотери";
+            this.теплопотериToolStripMenuItem.Click += new System.EventHandler(this.теплопотериToolStripMenuItem_Click);
             // 
             // EditorReferenceTables
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(697, 334);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "EditorReferenceTables";
             this.Text = "Редактирование справочных таблиц";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem температурыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem наружныйДиаментрToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem условныйДиаметрToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem объемВодыВСистемеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem теплопотериToolStripMenuItem;
     }
 }
--- heatLoss/heatLoss/EditorReferenceTables.cs
+++ heatLoss/heatLoss/EditorReferenceTables.cs
@@ -16,5 +16,40 @@ namespace heatLoss
         {
             InitializeComponent();
         }
+
+        private void температурыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TempRegion temp_region = new TempRegion();
+            temp_region.Owner = this;
+            temp_region.ShowDialog();
+        }
+
+        private void наружныйДиаментрToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OutsideDiameter outside_diameter = new OutsideDiameter();
+            outside_diameter.Owner = this;
+            outside_diameter.ShowDialog();
+        }
+
+        private void условныйДиаметрToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NominalDiameter nominal_diameter = new NominalDiameter();
+            nominal_diameter.Owner = this;
+            nominal_diameter.ShowDialog();
+        }
+
+        private void объемВодыВСистемеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SysWaterVolume sys_water_volume = new SysWaterVolume();
+            sys_water_volume.Owner = this;
+            sys_water_volume.ShowDialog();
+        }
+
+        private void теплопотериToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Heatloss heatloss = new Heatloss();
+            heatloss.Owner = this;
+            heatloss.ShowDialog();
+        }
     }
 }
