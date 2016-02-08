63a6b4c6b14e14e08da59afa65e6739d897f2a95
Spit437 <spit437@gmial.com>
2016-02-08 19:06:42 +0300

    Добавил класс для расчётов, описсал парочку ипеременных создал один метод. промежуточное сохранение

---
+++ heatLoss/heatLoss/claculationFirst.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heatLoss
+{
+    class claculationFirst
+    {
+        double deltaTcv;    //Температурный напор подземная прокладка, Цельсий
+        double t1_crG;      //среднегодовая температура сетевой воды в подающем и обратном тру-бопроводах определенные 
+        double t2_crG;      //из проектного температурного графика, градус Цельсия
+        double taoGr_crG;   //среднегодовая температура грунта согласно ТАБЛИЦЫ КОТОРУЮ НУЖНО ДАБОАВИТЬ, Цельсий
+
+        
+
+
+
+        //Интерполяция для двухтрубной подземной прокладки
+        public double twoTubesUnderground(double t1_crG, double t2_crG, double taoGr_crG)
+        {
+            return deltaTcv = ((t1_crG + t2_crG) / 2) - taoGr_crG;
+        }
+
+
+        //для подающего отдельно
+        public double onePipeUndergtoundFlow()
+
+    }
+}
