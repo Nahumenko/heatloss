07b92937c4000360ffabff6629c48ee43d6f1dfd
Spit437 <spit437@gmial.com>
2016-02-10 18:34:58 +0300

    сделал класс который соответствует пятому рассчёту

--- heatLoss/heatLoss/claculationFirst.cs
+++ heatLoss/heatLoss/claculationFirst.cs
@@ -42,6 +42,43 @@ namespace heatLoss
         double Zotkl;           //часы отключения
         double QnOtkl_crG;      // потери отключения
         double qotkl;
+        double Qnorm_crG;       //Нормируемые часовые значения среднегодовых тепловых 
+        double Q1norm_crG;      //Нормируемые часовые значения среднегодовых тепловых 
+        double Q2norm_crG;      //Нормируемые часовые значения среднегодовых тепловых 
+        double K1;              //кафики
+        double K2;
+        double K;
+        double K3;
+        double SummQnorm_crG;    //Нормируемые часовые значения среднегодовых потерь по видам прокладок в целом 
+        double Qnorm;           //средние за расчетный период часовые нормируемые значения тепловых потерь 
+        double Q1norm_podz;     //подземная прокладка при отдельно уложенном трубопроводе,подача
+        double Q2norm_podz;     //подземная прокладка при отдельно уложенном трубопроводе, обратка
+        double t1_crPer;        //средние температуры сетевой воды в подающем и обрат-ном трубопроводах 
+        double t2_crPer;        //за рассматриваемый расчетный период, цельсий
+        double taoGr_crPer;     //средняя температура грунта за рассматриваемый расчетный период
+        double taoV_crPer;      //средняя температура воздуха за рассматриваемый расчетный период
+        double Q1norm_vozd;     //подающий трубопровод   надземная прокладка
+        double Q2norm_vozd;     //обратный трубопровод надземная прокладка
+        double Q1norm_pom;      //прокладка в помещении подающий трубопровод
+        double Q2norm_pom;      //прокладка в помещении обратный трубопровод
+        double Q1norm_tunnel;      //прокладка в тунеле подающий трубопровод
+        double Q2norm_tunnel;      //прокладка в тунеле обратный трубопровод
+        double SummQ1norm_crGPodz;
+        double SummQ2norm_crGPodz;
+        double SummQ1norm_crGVozd;
+        double SummQ2norm_crGVozd;
+        double SummQ1norm_crGPom;
+        double SummQ2norm_crGPom;
+        double SummQ1norm_crGTunnel;
+        double SummQ2norm_crGTunnel;
+
+
+
+
+
+
+
+
 
 
 
@@ -165,5 +202,88 @@ namespace heatLoss
         {
            return QnOtkl_crG = qotkl * Beta * L * Zotkl / Z_per;
         }
+
+        //Нормирование тепловых потерь через теплоизоляционные конструк-ции трубопроводов водяных тепловых сетей
+        //для подземной двухтрубной прокладки  
+        public double NormAvarageAnnualHeatLossUnderground(double Qn_crG, double K)
+        {
+           return Qnorm_crG = Qn_crG * K;
+        }
+
+        //для участков надземной прокладки, прокладки в помещениях (технических подпо-льях), 
+        //тоннелях (проходных каналах), а также отдельно уложенных трубопроводов подземной прокладки:
+        public double NormAvarageAnnualHeatLoss(double K1, double K2, double Q1n_crG, double Q2n_crG,Direction direction)
+        {
+            if (direction == Direction.FLOW)
+            {
+                return Q1norm_crG = Q1n_crG * K1;
+            }
+            else
+            {
+                return Q2norm_crG = Q2n_crG * K2;
+            }
+        }
+
+        //средние за расчетный период часовые нормируемые значения тепловых потерь пу-тем пересчета нормируемых
+        //среднегодовых потерь с расчетного (проектного) сред-негодового режима на режим работы теплосети в расчетном периоде
+        //двухтрубная подземная прокладка
+        public double twoTibesUndergroundNormAvarege(double SummQnorm_crG, double t1_crPer, double t2_crPer, double taoGr_crPer, double t1_crG, double t2_crG,double taoGr_crG)
+        {
+            return Qnorm = SummQnorm_crG * (t1_crPer + t2_crPer - 2 * taoGr_crPer) / (t1_crG + t2_crG - 2 * taoGr_crG);
+        }
+
+        //подземная прокладка при отдельно уложенном трубопроводе
+       public double onePipeUndergroundNormalAvarage(double SummQ1norm_crGPodz, double SummQ2norm_crGPodz, double t1_crPer, double t2_crPer, double taoGr_crPer, double t1_crG, double t2_crG, double taoGr_crG, Direction direction)
+        {
+            if (direction == Direction.FLOW)
+            {
+                return Q1norm_podz = SummQ1norm_crGPodz * (t1_crPer - taoGr_crPer) / (t1_crG - taoGr_crG);
+            }
+            else
+            {
+                return Q2norm_podz = SummQ2norm_crGPodz * (t2_crPer - taoGr_crPer) / (t2_crG - taoGr_crG);
+            }
+        }
+
+        //надземная прокладка
+        public double onePipeAirNormalAvarage(double SummQ1norm_crGVozd, double SummQ2norm_crGVozd, double t1_crPer, double t2_crPer, double taoV_crPer, double t1_crG, double t2_crG, double taoV_crG, Direction direction)
+        {
+            if (direction == Direction.FLOW)
+            {
+                return Q1norm_vozd = SummQ1norm_crGVozd * (t1_crPer - taoV_crPer) / (t1_crG - taoV_crG);
+            }
+            else
+            {
+                return Q2norm_vozd = SummQ2norm_crGVozd * (t2_crPer - taoV_crPer) / (t2_crG - taoV_crG);
+            }
+        }
+
+        //прокладка в помещении
+        public double onePipeHouseNormalAvarage(double SummQ1norm_crGPom, double SummQ2norm_crGPom, double t1_crPer, double t2_crPer, double t1_crG, double t2_crG, Direction direction)
+        {
+            if (direction == Direction.FLOW)
+            {
+                return Q1norm_pom = SummQ1norm_crGPom * (t1_crPer - 15) / (t1_crG - 15);
+            }
+            else
+            {
+                return Q2norm_pom = SummQ2norm_crGPom * (t2_crPer - 15) / (t2_crG - 15);
+            }
+        }
+
+        //прокладка в канале
+        public double onePipeTunnelNormalAvarage(double SummQ1norm_crGPom, double SummQ2norm_crGPom, double t1_crPer, double t2_crPer, double t1_crG, double t2_crG, Direction direction)
+        {
+            if (direction == Direction.FLOW)
+            {
+                return Q1norm_tunnel = SummQ1norm_crGTunnel * (t1_crPer - 40) / (t1_crG - 40);
+            }
+            else
+            {
+                return Q2norm_tunnel = SummQ2norm_crGTunnel * (t2_crPer - 40) / (t2_crG - 40);
+            }
+        }
+        
+
     }
 }
