3ce30f59f7d5c1a1a76c6183a85ba74f7ab230ed
Spit437 <spit437@gmial.com>
2016-03-25 17:55:29 +0300

    полностью готовое окно авторизации

--- heatLoss/heatLoss/Authorization.Designer.cs
+++ heatLoss/heatLoss/Authorization.Designer.cs
@@ -40,10 +40,10 @@
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonLogin.Location = new System.Drawing.Point(313, 85);
+            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.Location = new System.Drawing.Point(340, 78);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(160, 68);
+            this.buttonLogin.Size = new System.Drawing.Size(133, 79);
             this.buttonLogin.TabIndex = 0;
             this.buttonLogin.Text = "Войти";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -51,43 +51,45 @@
             // 
             // textBUserName
             // 
-            this.textBUserName.Location = new System.Drawing.Point(108, 82);
+            this.textBUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBUserName.Location = new System.Drawing.Point(143, 78);
             this.textBUserName.Name = "textBUserName";
-            this.textBUserName.Size = new System.Drawing.Size(170, 20);
+            this.textBUserName.Size = new System.Drawing.Size(170, 38);
             this.textBUserName.TabIndex = 1;
             // 
             // textBPass
             // 
-            this.textBPass.Location = new System.Drawing.Point(108, 133);
+            this.textBPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBPass.Location = new System.Drawing.Point(143, 119);
             this.textBPass.Name = "textBPass";
-            this.textBPass.Size = new System.Drawing.Size(170, 20);
+            this.textBPass.Size = new System.Drawing.Size(170, 38);
             this.textBPass.TabIndex = 2;
             this.textBPass.TextChanged += new System.EventHandler(this.textBPass_TextChanged);
             // 
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
-            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblUserName.Location = new System.Drawing.Point(35, 82);
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblUserName.Location = new System.Drawing.Point(12, 81);
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(48, 20);
+            this.lblUserName.Size = new System.Drawing.Size(83, 31);
             this.lblUserName.TabIndex = 3;
             this.lblUserName.Text = "Имя :";
             // 
             // lblPass
             // 
             this.lblPass.AutoSize = true;
-            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblPass.Location = new System.Drawing.Point(8, 133);
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPass.Location = new System.Drawing.Point(9, 122);
             this.lblPass.Name = "lblPass";
-            this.lblPass.Size = new System.Drawing.Size(75, 20);
+            this.lblPass.Size = new System.Drawing.Size(123, 31);
             this.lblPass.TabIndex = 4;
             this.lblPass.Text = "Пароль :";
             // 
             // lblCheckConnection
             // 
             this.lblCheckConnection.AutoSize = true;
-            this.lblCheckConnection.Location = new System.Drawing.Point(12, 9);
+            this.lblCheckConnection.Location = new System.Drawing.Point(75, 175);
             this.lblCheckConnection.Name = "lblCheckConnection";
             this.lblCheckConnection.Size = new System.Drawing.Size(0, 13);
             this.lblCheckConnection.TabIndex = 5;
@@ -95,10 +97,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(184, 45);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(112, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 24);
+            this.label1.Size = new System.Drawing.Size(280, 39);
             this.label1.TabIndex = 6;
             this.label1.Text = "АВТОРИЗАЦИЯ";
             // 
@@ -106,7 +108,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 190);
+            this.ClientSize = new System.Drawing.Size(529, 197);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblCheckConnection);
             this.Controls.Add(this.lblPass);
@@ -115,7 +117,13 @@
             this.Controls.Add(this.textBUserName);
             this.Controls.Add(this.buttonLogin);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(545, 235);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(545, 235);
             this.Name = "Authorization";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Авторизация";
             this.Load += new System.EventHandler(this.Authorization_Load);
             this.ResumeLayout(false);
